b457134368df02a31260e32706a30c9738b9f5d1
jay <keangjay@gmail.com>
2021-04-19 15:46:55 +0800

    使用 typeorm 连接数据库

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 import "reflect-metadata";
 import {createConnection} from "typeorm";
-import {User} from "./entity/User";
+// import {User} from "./entity/User";
 
 createConnection().then(async connection => {
 
-    console.log("Inserting a new user into the database...");
+    console.log(connection);
     connection.close();
 
 }).catch(error => console.log(error));
