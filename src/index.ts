c33b556f2fdbc761d659719c4eecb80387f92ff4
jay <keangjay@gmail.com>
2021-04-26 16:33:45 +0800

    使用 manager 操作实体

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,17 @@
 import "reflect-metadata";
 import {createConnection} from "typeorm";
-// import {User} from "./entity/User";
+import {Post} from "./entity/Post";
 
 createConnection().then(async connection => {
 
-    console.log(connection);
+    const posts = await connection.manager.find(Post)
+    console.log(posts);
+    const p = new Post();
+    p.title = 'first';
+    p.content = 'content';
+    await connection.manager.save(p);
+    const posts2 = await connection.manager.find(Post)
+    console.log(posts2);
     connection.close();
 
 }).catch(error => console.log(error));
