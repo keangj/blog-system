a9cc63adf2e5d42294bcf6d4f9d0a8f3fd2db658
jay <keangjay@gmail.com>
2021-04-26 17:09:13 +0800

    使用 seed 创建数据

--- src/entity/Post.ts
+++ src/entity/Post.ts
@@ -8,4 +8,8 @@ export class Post {
   title: string;
   @Column('text')
   content: string;
+  // Partial 可传部分属性
+  constructor(attributes: Partial<Post>) {
+    Object.assign(this, attributes);
+  }
 }
--- src/seed.ts
+++ src/seed.ts
@@ -6,12 +6,9 @@ createConnection().then(async connection => {
 
     const posts = await connection.manager.find(Post)
     console.log(posts);
-    const p = new Post();
-    p.title = 'first';
-    p.content = 'content';
-    await connection.manager.save(p);
-    const posts2 = await connection.manager.find(Post)
-    console.log(posts2);
+    await connection.manager.save([1,2,3,4,5].map(item => {
+        return new Post({ title: `第${item}篇标题`, content: `第${item}篇内容` });
+    }));
     connection.close();
 
 }).catch(error => console.log(error));
