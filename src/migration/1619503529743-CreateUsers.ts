b3276e66c8b53e8f4b814fff5aff62171e78553c
jay <keangjay@gmail.com>
2021-04-27 14:41:53 +0800

    创建 users posts comments 表

---
+++ src/migration/1619503529743-CreateUsers.ts
@@ -0,0 +1,20 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class CreateUsers1619503529743 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(new Table({
+            name: 'users',
+            columns: [
+                { name: 'id', type: 'int', isPrimary: true, isGenerated: true, generationStrategy: 'increment' },
+                { name: 'username', type: 'varchar' },
+                { name: 'password_digest', type: 'varchar' }
+            ]
+        }));
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('users');
+    }
+
+}
---
+++ src/migration/1619503912795-CreatePosts.ts
@@ -0,0 +1,21 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class CreatePosts1619503912795 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(new Table({
+            name: 'posts',
+            columns: [
+                { name: 'id', type: 'int', isPrimary: true, isGenerated: true, generationStrategy: 'increment' },
+                { name: 'title', type: 'varchar' },
+                { name: 'content', type: 'text' },
+                { name: 'author_id', type: 'int' }
+            ]
+        }));
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('posts');
+    }
+
+}
