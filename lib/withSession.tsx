81334cd1b4dd4fce50b72ad27af446c2e23e8e6d
jay <keangjay@gmail.com>
2021-05-13 10:33:50 +0800

    使用 env.local 设置密码

--- lib/withSession.tsx
+++ lib/withSession.tsx
@@ -3,8 +3,7 @@ import { withIronSession } from "next-iron-session";
 
 export function withSession (handler: NextApiHandler) {
   return withIronSession(handler, {
-    // password: process.env.SECRET_COOKIE_PASSWORD,
-    password: '34438372-c01d-4e79-aba6-3de5a7425c5a',
+    password: process.env.SECRET,
     cookieName: 'blog',
     cookieOptions: {
       secure: false
