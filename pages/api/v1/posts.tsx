4baaa89669d2e58dcba2a66739e0c16a2e263cc8
jay <keangjay@gmail.com>
2021-05-24 16:16:14 +0800

    添加文章功能

--- pages/api/v1/posts.tsx
+++ pages/api/v1/posts.tsx
@@ -1,13 +1,19 @@
-import { getPosts } from "lib/posts";
+import { getDatabaseConnection } from "lib/getDatabaseConnection";
+import { withSession } from "lib/withSession";
 import { NextApiHandler } from "next";
+import { Post } from "src/entity/Post";
 
-const Posts: NextApiHandler = async (req, res) => {
-  const posts = await getPosts()
-  console.log(posts);
-  res.statusCode = 200
-  res.setHeader('Content-Type', 'application/json')
-  res.json(JSON.stringify(posts))
-  // res.json({ name: 'John Doe' })
-  res.end()
-}
+const Posts: NextApiHandler = withSession(async (req, res) => {
+  const connection = await getDatabaseConnection();
+  const user = req.session.get('currentUser');
+  if (req.method === 'POST') {
+    const { title, content } = req.body;
+    const post = new Post();
+    post.title = title;
+    post.content = content;
+    post.author = user
+    await connection.manager.save(post);
+    res.json(post)
+  }
+})
 export default Posts
