16e7161a4fc89eac02d9debba35869fa1c4fdbbb
jay <keangjay@gmail.com>
2021-05-27 16:08:21 +0800

    修复 next-env.d.ts 类型无法全局使用问题

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,20 +1,12 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
-
+// 这个文件有 import 那么这个文件就不是默认全局
 import * as next from 'next';
 import { NextApiRequest } from 'next';
 import { Session } from 'next-iron-session';
 declare module "*.jpg" {
   const value: string;
   export default value;
-}
-
-type Post = {
-  id: string;
-  title: string;
-  date: string;
-  content: string;
-  htmlContent: string;
 }
 
 declare module "next" {
