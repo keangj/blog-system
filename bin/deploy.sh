e84b418276bb8a1800bf3f704382bd23f34a5c0b
jay <keangjay@gmail.com>
2021-06-02 16:33:58 +0800

    修改 deploy 文件

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,1 +1,9 @@
-echo hello > /tmp/frank
+cd /home/ubuntu/app/ &&
+git pull &&
+yarn install --production=false &&
+yarn build &&
+docker build . -t jay/node-web-app &&
+docker kill app &&
+docker rm app &&
+docker run --name --network=host -p 3000:3000 -d jay/node-web-app &&
+echo 'OK!'
