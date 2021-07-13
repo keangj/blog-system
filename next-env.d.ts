97d7600d2f6c3f242f3e969f54cddd10f20bfc38
jay <keangjay@gmail.com>
2021-07-13 10:37:54 +0800

    升级版本

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,16 +1,3 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
-// 这个文件有 import 那么这个文件就不是默认全局
-import * as next from 'next';
-import { NextApiRequest } from 'next';
-import { Session } from 'next-iron-session';
-declare module "*.jpg" {
-  const value: string;
-  export default value;
-}
-
-declare module "next" {
-  interface NextApiRequest {
-    session: Session
-  }
-}
+/// <reference types="next/image-types/global" />
